f653ce5ecda01d781e0886c1ddda1081e23fdb51
Fayaz Yusuf Khan <fayaz.yusuf.khan@gmail.com>
2025-04-16 19:48:11 -0400

    Add __main__

---
+++ src/task_orch/__main__.py
@@ -0,0 +1,11 @@
+import argparse
+from task_orch.session_coordinator import SessionCoordinator
+
+
+parser = argparse.ArgumentParser(description="Distributed Task Orchestration Demo")
+parser.add_argument("--session-id", type=str, required=True)
+parser.add_argument("--tasks", nargs="+", required=True)
+args = parser.parse_args()
+
+coordinator = SessionCoordinator()
+coordinator.run_session(args.session_id, args.tasks)
